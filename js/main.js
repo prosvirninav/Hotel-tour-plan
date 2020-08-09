7735986af0386b10aa21bf0295e300d82f0dccb3
prosvirninav <prosvirninav@mail.ru>
2020-08-09 17:28:43 +0300

    fix: final revisions

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ $(document).ready(function () {
         },
         tel: {
           required: "Phone is required",
-          minlength: "Longer than 11 symbol",
+          minlength: "Longer than 11 digits",
         },
       },
     });
@@ -79,4 +79,7 @@ $(document).ready(function () {
       '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15779.35919615496!2d98.22963715616208!3d8.611374924883686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3050ee61a0a98b25%3A0xa37a5d4fd305d46c!2sThe%20Briza%20Beach%20Resort%2C%20Khao%20Lak!5e0!3m2!1sru!2sru!4v1596839796358!5m2!1sru!2sru" class="map-frame" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>'
     );
   });
+
+  $(".phone").mask("+7(000) 000-0000")
+
 });
